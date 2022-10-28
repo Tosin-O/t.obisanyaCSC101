2d384b1a8edc8a1758f98f4e32d47d91b6a1d6ab
Tosin <115807118+Tosin-O@users.noreply.github.com>
2022-10-28 15:37:18 +0100

    Add files via upload

---
+++ week-2/Project_I.rs
@@ -0,0 +1,16 @@
+int main()
+{
+    // We can change values here for
+    // different inputs
+    float P = 520_000, R = 10, T = 5;
+ 
+    /* Calculate simple interest */
+    float A = P*(1+(R/100));
+
+    float CI =A-P
+ 
+    /* Print the resultant value of CI */
+    cout << "Compound Interest = " << CI;
+ 
+    return 0;
+}
